f74503034175668578841c23ccb4aaa8d07c2494
Samrick <ricrdocardoso@gmail.com>
2020-12-05 12:10:19 -0300

    Atualizado SQL
    Atualizado SQL para criar as tabelas de 'produto', 'usuario' e 'telefone' já com autoincremento de 'id'

--- WebContent/resources/SQL/Tabelas.sql
+++ WebContent/resources/SQL/Tabelas.sql
@@ -1,5 +1,23 @@
+-- Criar o BD 'curso-jsp'
+/*
+CREATE DATABASE "curso-jsp"
+  WITH ENCODING='UTF8'
+       OWNER=postgres
+       CONNECTION LIMIT=-1
+       TABLESPACE=pg_default;
+*/
+
+-- inserir autoincremento para a coluna 'id' na tabela 'usuario' --
+CREATE SEQUENCE user_id_seq
+    INCREMENT 1
+    MINVALUE 1
+    MAXVALUE 9223372036854775807
+    START 1
+    CACHE 1;
+
+-- Criação tabela 'Usuario'
 CREATE TABLE usuario(
-  id BIGINT NOT NULL,
+  id BIGINT NOT NULL DEFAULT nextval('user_id_seq'::regclass),
   login character varying(255),
   senha character varying(255),
   nome character varying(255),
@@ -14,20 +32,41 @@ CREATE TABLE usuario(
   curriculobase64 character varying(255),
   contenttypecurriculo character varying(255),
   CONSTRAINT user_pkey PRIMARY KEY (id)
-)
+);
 
-CREATE TABLE produto(
-codigo bigint NOT NULL,
-nome character varying(255),
-preco FLOAT,
-estoque FLOAT,
-CONSTRAINT prod_pkey PRIMARY KEY (codigo)
-)
+-- Inserir autoincremento para a tabela produto, campo 'id'
+CREATE SEQUENCE prod_id_seq
+    INCREMENT 1
+    MINVALUE 1
+    MAXVALUE 9223372036854775807
+    START 1
+    CACHE 1;
 
+-- Criar tabela 'produto';
+
+CREATE TABLE public.produto
+(
+  id bigint NOT NULL DEFAULT nextval('prod_id_seq'::regclass),
+  nome character varying(255),
+  codigo bigint NOT NULL,
+  preco double precision,
+  estoque double precision,
+  CONSTRAINT prod_pkey PRIMARY KEY (codigo)
+);
+
+-- Inserir autoincremento para a tabela telefone, campo 'id'
+CREATE SEQUENCE tel_id_seq
+    INCREMENT 1
+    MINVALUE 1
+    MAXVALUE 9223372036854775807
+    START 1
+    CACHE 1;
+
+-- Criar tabela 'Telefone'
 CREATE TABLE telefone(
-  id BIGINT NOT NULL,
+  id BIGINT NOT NULL DEFAULT nextval('tel_id_seq'::regclass),
   numero character varying(20),
   tipo character varying(20),
-  usuario BIGINT 
+  usuario BIGINT,
   CONSTRAINT tel_pkey PRIMARY KEY (id)
-)
+);
